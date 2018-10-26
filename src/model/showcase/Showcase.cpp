ac8d9319f20b585eae7a1768739e15f947cbf290
alex peter charles lehmann <me.allega@gmail.com>
2018-10-26 11:24:56 +0200

    model structure

---
+++ src/model/showcase/Showcase.cpp
@@ -0,0 +1,5 @@
+//Creator:      Alex Lehmann
+//Date:         26.10.2018
+//Description:  This Class defines the whole Showcase
+
+
---
+++ src/model/tube/Tube.cpp
@@ -0,0 +1,3 @@
+//Creator:      Alex Lehmann
+//Date:         26.10.2018
+//Description:  This Class defines the LED Tubes
